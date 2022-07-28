7e70b9ecf61eb02c54201248fa4ed80ea408a348
Bruno Henrique Alves Nunes <brunorick32@hotmail.com>
2022-07-28 13:58:47 -0300

    Adicionado telas BrwMedicoes e FrmMedicoes.

---
+++ Academia/View/BrwMedicoes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Academia.View
+{
+    public partial class BrwMedicoes : Form
+    {
+        public BrwMedicoes()
+        {
+            InitializeComponent();
+        }
+
+        private void BrwMedicoes_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Academia/View/FrmMedicoes.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Academia.View
+{
+    partial class FrmMedicoes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FrmMedicoes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "FrmMedicoes";
+            this.Text = "FrmMedicoes";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
